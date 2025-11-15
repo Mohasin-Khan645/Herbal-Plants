f35496d54c257b84a5e649d2e9a40b3a4d2d28a5
Mohammad Mohasin Khan <kmohasin645@gmail.com>
2025-11-15 22:06:58 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // js/config.js
 // Configuration for Herbal Guardian
 
-window.HG_GEMINI_API_KEY = 'YOUR_GEMINI_API_KEY';
+window.HG_GEMINI_API_KEY = 'AIzaSyANw1Xe9x6SNQo8EoDiznCTZmpk-BvnOc0';
 
 
 
